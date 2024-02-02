a73f2f6f87ca995972974be0b9c14ab3ddbbd43b
sandeep-vaid <830508@sita.in>
2024-02-02 23:05:35 +0530

    Day 4

---
+++ Day3/variable.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main() {
+	var a = "Apple Computer";
+	fmt.Println(a);
+	var b int = 10;
+	fmt.Println(b);
+	//Here we declare a int variable but didn't give any value in that case its value is zero as default value. 
+	var c int ;
+	fmt.Println(c);
+	var d = false;
+	fmt.Println(d);
+	//A way to initialize and declare a variable;
+	e:= false;
+	fmt.Println(e);
+}
