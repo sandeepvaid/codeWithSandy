94fd9ec5694cd3d3d8e59c59c949386f004451da
sandeep-vaid <830508@sita.in>
2024-02-04 23:44:26 +0530

    Day 6

---
+++ Day3/condition.go
@@ -0,0 +1,11 @@
+package main 
+import "fmt"
+
+func main(){
+	var a = 10;
+	if a>5{
+		fmt.Println("10 is greater than 5");
+	}else if a> 10{
+		fmt.Println("10 is not greater than 5");
+	}
+}
---
+++ Day3/operator.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main(){
+	fmt.Println("operator");
+	var (
+		sums1 = 0;
+		sums2 = 1;
+	)
+	fmt.Println(sums1,sums2);
+}
